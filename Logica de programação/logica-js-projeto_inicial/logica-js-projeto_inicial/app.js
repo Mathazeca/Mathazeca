a4545464d0991bebc55f24e848b5aed778a2bfce
Matheus Carvalho <mathazecarvalho@gmail.com>
2024-03-20 17:45:57 -0300

    projeto inicial

---
+++ Logica de programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+
+alert('oi, puta. Espero que perca!');
+let numeroSecreto = 4;
+let chute = prompt('Escolha um número de 1-10');
+console.log(numeroSecreto)
+if(numeroSecreto == chute) {
+    alert(`Que merda! Você descobriu ${numeroSecreto}.`);
+} else {
+    alert('Que ótimo! Você errou.');
+}
